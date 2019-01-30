241c4ac7ca2b8d175fd8244c29e149cc37a8f7fa
Ajmal Afif <ajmalafif@gmail.com>
2019-01-30 22:26:11 +0800

    Slowly but surely

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -6,6 +6,15 @@ import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
 import Pricing from '../components/Pricing'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import tachyons from 'tachyons-components'
+
+const SectionHeader = tachyons('h1')`
+f1
+`
+
+const SectionSubHeader = tachyons("p")`
+  gray mt0
+`
 
 export const ProductPageTemplate = ({
   image,
@@ -18,94 +27,63 @@ export const ProductPageTemplate = ({
   fullImage,
   pricing,
 }) => (
-  <section className="section section--gradient">
-    <div className="container">
-      <div className="section">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="content">
-              <div
-                className="full-width-image-container margin-top-0"
-                style={{
-                  backgroundImage: `url(${
-                    !!image.childImageSharp
-                      ? image.childImageSharp.fluid.src
-                      : image
-                  })`,
-                }}
-              >
-                <h2
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  {title}
-                </h2>
-              </div>
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    {heading}
-                  </h3>
-                  <p>{description}</p>
-                </div>
-              </div>
-              <Features gridItems={intro.blurbs} />
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-3">
-                    {main.heading}
-                  </h3>
-                  <p>{main.description}</p>
-                </div>
-              </div>
-              <div className="tile is-ancestor">
-                <div className="tile is-vertical">
-                  <div className="tile">
-                    <div className="tile is-parent is-vertical">
-                      <article className="tile is-child">
-                        <PreviewCompatibleImage imageInfo={main.image1} />
-                      </article>
-                    </div>
-                    <div className="tile is-parent">
-                      <article className="tile is-child">
-                        <PreviewCompatibleImage imageInfo={main.image2} />
-                      </article>
-                    </div>
-                  </div>
-                  <div className="tile is-parent">
-                    <article className="tile is-child">
-                      <PreviewCompatibleImage imageInfo={main.image3} />
-                    </article>
-                  </div>
-                </div>
-              </div>
-              <Testimonials testimonials={testimonials} />
-              <div
-                className="full-width-image-container"
-                style={{
-                  backgroundImage: `url(${
-                    fullImage.childImageSharp
-                      ? fullImage.childImageSharp.fluid.src
-                      : fullImage
-                  })`,
-                }}
-              />
-              <h2 className="has-text-weight-semibold is-size-2">
-                {pricing.heading}
-              </h2>
-              <p className="is-size-5">{pricing.description}</p>
-              <Pricing data={pricing.plans} />
-            </div>
+  <div className="w-100 db wrap">
+    <SectionHeader>
+      {title}
+      <h3 className="has-text-weight-semibold is-size-2">
+        {heading}
+      </h3>
+    </SectionHeader>
+      <SectionSubHeader>
+        <p>{description}</p>
+      </SectionSubHeader>
+    <Features gridItems={intro.blurbs} />
+    <div className="columns">
+      <div className="column is-7">
+        <h3 className="has-text-weight-semibold is-size-3">
+          {main.heading}
+        </h3>
+        <p>{main.description}</p>
+      </div>
+    </div>
+    <div className="tile is-ancestor">
+      <div className="tile is-vertical">
+        <div className="tile">
+          <div className="tile is-parent is-vertical">
+            <article className="tile is-child">
+              <PreviewCompatibleImage imageInfo={main.image1} />
+            </article>
           </div>
+          <div className="tile is-parent">
+            <article className="tile is-child">
+              <PreviewCompatibleImage imageInfo={main.image2} />
+            </article>
+          </div>
+        </div>
+        <div className="tile is-parent">
+          <article className="tile is-child">
+            <PreviewCompatibleImage imageInfo={main.image3} />
+          </article>
         </div>
       </div>
     </div>
-  </section>
+    <Testimonials testimonials={testimonials} />
+    <div
+      className="full-width-image-container"
+      style={{
+        backgroundImage: `url(${
+          fullImage.childImageSharp
+            ? fullImage.childImageSharp.fluid.src
+            : fullImage
+        })`,
+      }}
+    />
+    <h2 className="has-text-weight-semibold is-size-2">
+      {pricing.heading}
+    </h2>
+    <p className="is-size-5">{pricing.description}</p>
+    <Pricing data={pricing.plans} />
+  </div>
 )
 
 ProductPageTemplate.propTypes = {
