e8a36f615ad44b3c5a32d638a0010aae430ab57c
Ajmal Afif <ajmalafif@gmail.com>
2019-02-04 00:22:01 +0800

    Remove isRequired for companies

--- src/components/PreviewFixedImage.js
+++ src/components/PreviewFixedImage.js
@@ -26,9 +26,9 @@ PreviewFixedImage.propTypes = {
   imageInfo: PropTypes.shape({
     alt: PropTypes.string,
     childImageSharp: PropTypes.object,
-    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     style: PropTypes.object,
-  }).isRequired,
+  }),
 }
 
 export default PreviewFixedImage
