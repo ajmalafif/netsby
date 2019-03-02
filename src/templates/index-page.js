ec932c3d94a6c11d6b2209c7c21f992fc3483e49
Ajmal Afif <ajmalafif@gmail.com>
2019-03-03 02:34:27 +0800

    load properly

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -53,8 +53,8 @@ export const IndexPageTemplate = ({
         <ContainerMain>
           <Heading>Hi, I‘m Ajmal <span role="img" aria-label="emoji">👋🏼</span></Heading>
           <PageContent className="lh-copy content" content={content} />
-          <Link to="/experience/" className="link br2 ph4 pv3 dib mr3-ns white bg-primary db w-auto-ns w-100 tc">{primaryButton}</Link>
-          <Link to="/about/" className="link br2 ph4 pv3 mt2 dib blue bg-white tc w-auto-ns w-100 bg-secondary bs-secondary br-secondary">{secondaryButton}</Link>
+          <Link to="/experience/" className="link br2 ph4 pv3 dib mr3-ns white bg-primary db w-100 w-auto-ns tc">{primaryButton}</Link>
+          <Link to="/about/" className="link br2 ph4 pv3 mt2 dib blue bg-white tc w-100 w-auto-ns bg-secondary bs-secondary br-secondary">{secondaryButton}</Link>
         </ContainerMain>
       </Container>
     </Section>
