d3fff5d2c32be2daf4476953bd8fc10ad1d6f2fd
Ajmal Afif <ajmalafif@gmail.com>
2019-03-03 02:53:36 +0800

    width for auto-ns

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -124,12 +124,6 @@ const GlobalStyle = createGlobalStyle`
       font-size: 1.125rem;
     }
   }
-  
-  @media screen and (max-width: 30em) {
-    .w-auto-ns {
-      width: auto;
-    }
-  }
 `;
 
 const TemplateWrapper = ({ children }) => (
