6a05b5815ef19b767e0071e0b083808d3c709faf
Ajmal Afif <ajmalafif@gmail.com>
2019-03-05 02:21:01 +0800

    Thanks js

--- src/pages/about/thanks.js
+++ src/pages/about/thanks.js
@@ -9,8 +9,8 @@ link fw6
 
 export default () => (
   <Layout>
-    <div className="wrap">
-    <section className="blog w-100 ph3 ph0-ns pt3-ns mt4 mt5-ns">
+    <div className="wrap ph3 ph0-ns ">
+    <section className="blog w-100 pt3-ns mt4 mt5-ns">
       <h1 className="f3 fw6 mb2">Thank you!</h1>
       <p>I'll get back to you when I have the chance. You can also reach me on twitter, <a without rel="noopener noreferrer" target="_blank" className="link" href="https://twitter.com/ajmalafif">@ajmalafif</a>.</p>
       <p className="lh-copy">
