e52eb1ffedbd66649e8d3d179e31c892ca8563d0
Ajmal Afif <ajmalafif@gmail.com>
2019-03-05 02:21:32 +0800

    404 on wrapper

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,8 +9,8 @@ link fw6
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="wrap">
-    <section className="blog w-100 ph3 ph0-ns pt3-ns mt4 mt5-ns">
+    <div className="wrap ph3 ph0-ns">
+    <section className="blog w-100 pt3-ns mt4 mt5-ns">
       <h1 className="f3 fw6 mb2">How did you end up here?</h1>
       <p className="lh-copy">Opsie, the page you're looking for isn't here anymore. Sorry about that 🙏🏻 If this page contains something important to you, please do <a without rel="noopener noreferrer" target="_blank" className="link" href="/about/">let me know here</a>.</p>
       <p className="lh-copy">
