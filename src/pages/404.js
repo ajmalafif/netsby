3f6bc9fb8c6b9b0509b112100895f1dba173999e
Ajmal Afif <ajmalafif@gmail.com>
2019-03-05 01:53:28 +0800

    404 ready

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react"
+import { Link } from 'gatsby'
+import tachyons from 'tachyons-components'
+import Layout from '../../components/Layout'
+
+const BackLink = tachyons(Link)`
+link fw6
+`
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section className="blog w-100 wrap mt4 mt5-ns ph3 ph0-ns pt3-ns">
+      <h1 className="f3 fw6 mb2">Error 404</h1>
+      <p>The page you're looking for is no longer exist.</p>
+      <BackLink to="/">← Back to Home</BackLink>
+    </section>
   </Layout>
 )
 
