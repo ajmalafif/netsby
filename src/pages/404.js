2f7e03d04da9fbc488ba902dceaaedafb5c6d21d
Ajmal Afif <ajmalafif@gmail.com>
2019-03-05 03:29:00 +0800

    404 first

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,24 +3,32 @@ import { Link } from 'gatsby'
 import tachyons from 'tachyons-components'
 import Layout from '../components/Layout'
 
+const Wrapper = tachyons(`div`)`
+wrap ph3 ph0-ns
+`
+
+const Section = tachyons(`section`)`
+blog w-100 pt3-ns mt4 mt5-ns
+`
+
 const BackLink = tachyons(Link)`
 link fw6
 `
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="wrap ph3 ph0-ns">
-    <section className="blog w-100 pt3-ns mt4 mt5-ns">
+    <Wrapper>
+    <Section>
       <h1 className="f3 fw6 mb2">How did you end up here?</h1>
-      <p className="lh-copy">Opsie, the page you're looking for isn't here anymore. Sorry about that 🙏🏻 If this page contains something important to you, please do <a without rel="noopener noreferrer" target="_blank" className="link" href="/about/">let me know here</a>.</p>
+      <p className="lh-copy">Opsie, the page you're looking for isn't here anymore. Sorry about that <span role="img" aria-label="emoji">🙏🏻</span> If this page contains something important to you, please do <a without rel="noopener noreferrer" target="_blank" className="link" href="/about/">let me know here</a>.</p>
       <p className="lh-copy">
       <BackLink to="/about/">← Let me know</BackLink>
       </p>
       <p className="lh-copy">
       <BackLink to="/blog/">← Check out my articles</BackLink>
       </p>
-    </section>
-    </div>
+    </Section>
+    </Wrapper>
   </Layout>
 )
 
--- src/pages/about/thanks.js
+++ src/pages/about/thanks.js
@@ -9,7 +9,7 @@ link fw6
 
 export default () => (
   <Layout>
-    <div className="wrap ph3 ph0-ns ">
+    <div className="wrap ph3 ph0-ns">
     <section className="blog w-100 pt3-ns mt4 mt5-ns">
       <h1 className="f3 fw6 mb2">Thank you!</h1>
       <p>I'll get back to you when I have the chance. You can also reach me on twitter, <a without rel="noopener noreferrer" target="_blank" className="link" href="https://twitter.com/ajmalafif">@ajmalafif</a>.</p>
