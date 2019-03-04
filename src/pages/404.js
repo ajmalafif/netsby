07a8e91fa1fadfaa1042fc547bfed6edaa35d711
Ajmal Afif <ajmalafif@gmail.com>
2019-03-05 02:04:35 +0800

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from 'gatsby'
 import tachyons from 'tachyons-components'
-import Layout from '../../components/Layout'
+import Layout from '../components/Layout'
 
 const BackLink = tachyons(Link)`
 link fw6
@@ -10,9 +10,14 @@ link fw6
 const NotFoundPage = () => (
   <Layout>
     <section className="blog w-100 wrap mt4 mt5-ns ph3 ph0-ns pt3-ns">
-      <h1 className="f3 fw6 mb2">Error 404</h1>
-      <p>The page you're looking for is no longer exist.</p>
-      <BackLink to="/">← Back to Home</BackLink>
+      <h1 className="f3 fw6 mb2">How did you end up here?</h1>
+      <p className="lh-copy">Opsie, the page you're looking for isn't here anymore. Sorry about that 🙏🏻 If this page contains something important to you, please do <a without rel="noopener noreferrer" target="_blank" className="link" href="/about/">let me know here</a>.</p>
+      <p className="lh-copy">
+      <BackLink to="/about/">← Let me know</BackLink>
+      </p>
+      <p className="lh-copy">
+      <BackLink to="/blog/">← Check out my articles</BackLink>
+      </p>
     </section>
   </Layout>
 )
