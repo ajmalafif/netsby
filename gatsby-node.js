31a3efd8ac3958be59d7a9e246a6bcc03c4cc6a7
Ajmal Afif <ajmalafif@gmail.com>
2019-03-28 14:23:29 +0800

    First try at pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,9 +30,27 @@ exports.createPages = ({ actions, graphql }) => {
       return Promise.reject(result.errors)
     }
 
-    const posts = result.data.allMarkdownRemark.edges
+    const postsAndPages = result.data.allMarkdownRemark.edges
 
-    posts.forEach(edge => {
+    // Post pages:
+    let posts = []
+    // Iterate through each post/page, putting all found posts (where templateKey = article-page) into `posts`
+    postsAndPages.forEach(edge => {
+      if (_.isMatch(edge.node.frontmatter, { 'templateKey': 'blog-post' })) {
+        posts = posts.concat(edge)
+      }
+    })
+
+    createPaginatedPages({
+      edges: posts,
+      createPage: createPage,
+      pageTemplate: 'src/templates/blog.js',
+      pageLength: 6, // This is optional and defaults to 10 if not used
+      pathPrefix: 'blog', // This is optional and defaults to an empty string if not used
+      context: {}, // This is optional and defaults to an empty object if not used
+    })
+
+    postsAndPages.forEach(edge => {
       const id = edge.node.id
       createPage({
         path: edge.node.fields.slug,
@@ -50,7 +68,7 @@ exports.createPages = ({ actions, graphql }) => {
     // Tag pages:
     let tags = []
     // Iterate through each post, putting all found tags into `tags`
-    posts.forEach(edge => {
+    postsAndPages.forEach(edge => {
       if (_.get(edge, `node.frontmatter.tags`)) {
         tags = tags.concat(edge.node.frontmatter.tags)
       }
