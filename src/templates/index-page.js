e5ef27d732b5350c4dc5049ea6cab25d68e746a6
Ajmal Afif <ajmalafif@gmail.com>
2019-04-10 22:59:02 +0800

    Experiment on same content for og:site_name and og:title

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -52,7 +52,14 @@ export const IndexPageTemplate = ({
         bodyAttributes={{
             class: 'homepage'
         }}
-      />
+      >
+        <meta name="description" content={content} />
+        <meta name="twitter:title" content={`Ajmal Afif`} />
+        <meta name="twitter:description" content={content} />
+        <meta property="og:site_name" content={`Ajmal Afif`} />
+        <meta property="og:title" content={`Ajmal Afif`} />
+        <meta property="og:description" content={content} />
+      </Helmet>
       <Container>
         <ContainerTitle>
           <Title>{title}</Title>
